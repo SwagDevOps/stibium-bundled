e17feddd3623a13521f291c45dfde29bfc41fc2e
Dimitri Arrigoni <dimitri@arrigoni.me>
2021-01-12 10:10:00 +0100

    gems upgrade rubocop to rubocop ~> 1.0

--- gems.rb
+++ gems.rb
@@ -19,7 +19,7 @@ group :development do
 
   gem 'listen', '~> 3.1'
   gem 'rake', '~> 13.0'
-  gem 'rubocop', '~> 0.66'
+  gem 'rubocop', '~> 1.0'
   gem 'rugged', '~> 1.0'
   gem 'sys-proc', '~> 1.1', '>= 1.1.2'
   # repl ------------------------------------------------------------
