492b2808a83c5be25e452e42ccb200c54e12d10f
Dimitri Arrigoni <dimitri@arrigoni.me>
2021-01-12 17:26:10 +0100

    bundled (spec) add tests based on samples

--- spec/stibium/bundled_spec.rb
+++ spec/stibium/bundled_spec.rb
@@ -43,6 +43,9 @@ sham!(:bundled).builder.tap do |builder|
     :bundled_from.tap do |method|
       context '.methods' do
         it { expect(described_class.methods).to include(method) }
+      end
+
+      context '.public_methods' do
         it { expect(described_class.public_methods).not_to include(method) }
       end
 
@@ -51,6 +54,44 @@ sham!(:bundled).builder.tap do |builder|
         described_class.clone.dup.tap do |c|
           c.singleton_class.instance_eval { public method.to_sym }
         end.tap { |c| expect(c).to respond_to(method).with(1).arguments }
+      end
+    end
+  end
+end
+
+# samples -----------------------------------------------------------
+sham!(:samples).lister.call.map { |k, _| [k, sham!(:samples).builder.call(k)] }.to_h.each do |_, c|
+  describe c, :'stibium/bundled', :samples do
+    it { expect(described_class).to be_a(Stibium::Bundled) }
+
+    { bundled: [NilClass, Stibium::Bundled::Bundle], bundled?: [TrueClass, FalseClass] }.each do |method, types|
+      it { expect(described_class).to respond_to(method).with(0).arguments }
+
+      context ".#{method}.class" do
+        it do
+          expect(described_class.__send__(method).class).to satisfy("be in #{types}") { |x| types.include?(x) }
+        end
+      end
+    end
+  end
+end
+
+{
+  empty: [FalseClass, NilClass],
+  gemfile: [TrueClass, Stibium::Bundled::Bundle],
+  gemfile_old: [TrueClass, Stibium::Bundled::Bundle],
+  partial: [FalseClass, NilClass],
+  partial_old: [FalseClass, NilClass],
+  standalone: [TrueClass, Stibium::Bundled::Bundle]
+}.sort.each do |name, types|
+  sham!(:samples).builder.call(name).tap do |c|
+    describe c, :'stibium/bundled', :samples do
+      context '.bundled?' do
+        it { expect(described_class.bundled?).to be_a(types.fetch(0)) }
+      end
+
+      context '.bundled' do
+        it { expect(described_class.bundled).to be_a(types.fetch(1)) }
       end
     end
   end
