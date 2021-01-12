a85a11ef2ca6a784a467e2150c14d36e50eb61e6
Dimitri Arrigoni <dimitri@arrigoni.me>
2021-01-12 20:15:31 +0100

    bundle (spec) add some trivial tests

--- spec/stibium/bundled/bundle_spec.rb
+++ spec/stibium/bundled/bundle_spec.rb
@@ -2,16 +2,35 @@
 
 # class methods -----------------------------------------------------
 describe Stibium::Bundled::Bundle, :'stibium/bundled/bundle' do
+  # @type [Class<Stibium::Bundled::Bundle]>] described_class
   it { expect(described_class).to respond_to(:new).with(1).arguments }
 end
 
 # instance methods --------------------------------------------------
 describe Stibium::Bundled::Bundle, :'stibium/bundled/bundle' do
-  let(:subject) { described_class.new(__dir__) }
+  # @type [String] basedir
+  # @type [Stibium::Bundled::Bundle] subject
+  # @type [Class<Stibium::Bundled::Bundle]>] described_class
+  let(:basedir) { __dir__ }
+  let(:subject) { described_class.new(basedir) }
 
   it { expect(subject).to be_frozen }
 
   [:bundled?, :to_path, :locked?, :gemfile, :gemfiles, :standalone?, :standalone!].each do |method|
     it { expect(subject).to respond_to(method).with(0).arguments }
+  end
+
+  context '#path' do
+    it { expect(subject.path).to be_frozen }
+
+    it do
+      expect(subject.path).to be_a(Pathname)
+      expect(subject.path).to eq(Pathname.new(basedir))
+    end
+  end
+
+  context '#to_path' do
+    it { expect(subject.to_path).to be_a(String) }
+    it { expect(subject.to_path).to eq(basedir) }
   end
 end
