660c82c0cabd23a15b82e2cc995f9b2962b35eb1
Dimitri Arrigoni <dimitri@arrigoni.me>
2021-01-13 15:55:13 +0100

    bundle (spec) use have_constant matcher + minor changes

--- spec/stibium/bundled/bundle_spec.rb
+++ spec/stibium/bundled/bundle_spec.rb
@@ -1,5 +1,14 @@
 # frozen_string_literal: true
 
+# constants ---------------------------------------------------------
+describe Stibium::Bundled::Bundle, :'stibium/bundled/bundle' do
+  [
+    :Config,
+  ].each do |k|
+    it { expect(described_class).to have_constant(k) }
+  end
+end
+
 # class methods -----------------------------------------------------
 describe Stibium::Bundled::Bundle, :'stibium/bundled/bundle' do
   # @type [Class<Stibium::Bundled::Bundle]>] described_class
@@ -14,11 +23,27 @@ describe Stibium::Bundled::Bundle, :'stibium/bundled/bundle' do
   let(:basedir) { __dir__ }
   let(:subject) { described_class.new(basedir) }
 
-  it { expect(subject).to be_frozen }
+  [:bundled?,
+   :to_path,
+   :locked?,
+   :gemfile,
+   :gemfiles,
+   :standalone?,
+   :standalone!,
+   :config,].each do |method|
+     it { expect(subject).to respond_to(method).with(0).arguments }
+   end
+end
 
-  [:bundled?, :to_path, :locked?, :gemfile, :gemfiles, :standalone?, :standalone!].each do |method|
-    it { expect(subject).to respond_to(method).with(0).arguments }
-  end
+# tests -------------------------------------------------------------
+describe Stibium::Bundled::Bundle, :'stibium/bundled/bundle' do
+  # @type [String] basedir
+  # @type [Stibium::Bundled::Bundle] subject
+  # @type [Class<Stibium::Bundled::Bundle]>] described_class
+  let(:basedir) { __dir__ }
+  let(:subject) { described_class.new(basedir) }
+
+  it { expect(subject).to be_frozen }
 
   context '#path' do
     it { expect(subject.path).to be_frozen }
@@ -32,5 +57,10 @@ describe Stibium::Bundled::Bundle, :'stibium/bundled/bundle' do
   context '#to_path' do
     it { expect(subject.to_path).to be_a(String) }
     it { expect(subject.to_path).to eq(basedir) }
+  end
+
+  context '#config' do
+    it { expect(subject.config).to be_a(Hash) }
+    it { expect(subject.config).to be_frozen }
   end
 end
