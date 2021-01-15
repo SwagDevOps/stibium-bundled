64e93b4d158f1329b709d1a47c18dcd52962e703
Dimitri Arrigoni <dimitri@arrigoni.me>
2021-01-15 10:15:54 +0100

    bundle (spec) config attributes test introduced

--- spec/stibium/bundled/bundle/config_spec.rb
+++ spec/stibium/bundled/bundle/config_spec.rb
@@ -2,9 +2,34 @@
 
 # class methods -----------------------------------------------------
 describe Stibium::Bundled::Bundle::Config, :'stibium/bundled/bundle/config' do
-  # @type [Class<Stibium::Bundled::Bundle::Config]>] described_class
+  # @type [Class<Stibium::Bundled::Bundle::Config>] described_class
   :new.tap do |method|
     it { expect(described_class).to respond_to(method).with(1).arguments }
     it { expect(described_class).to respond_to(method).with(1).arguments.with_keywords(:env) }
+  end
+end
+
+# attributes --------------------------------------------------------
+describe Stibium::Bundled::Bundle::Config, :'stibium/bundled/bundle/config' do
+  # @type [Class<Stibium::Bundled::Bundle::Config>] described_class
+  # @type [Stibium::Bundled::Bundle::Config] subject
+  # @type [Pathname] basedir
+  let(:basedir) { Pathname.new(__dir__) }
+  let(:subject) { described_class.new(basedir, env: {}) }
+
+  {
+    env: Hash,
+    basedir: Pathname,
+  }.each do |method, type|
+    it { expect(subject).to respond_to(method).with(0).arguments }
+
+    context "##{method}" do
+      it { expect(subject.public_send(method)).to be_a(type) }
+      it { expect(subject.public_send(method)).to be_frozen }
+
+      if :basedir == method.to_sym
+        it { expect(subject.basedir).to eq(basedir) }
+      end
+    end
   end
 end
