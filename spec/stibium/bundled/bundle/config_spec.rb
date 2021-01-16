ea67b3d1a90103e9c962e2067f0f12a506efbe76
Dimitri Arrigoni <dimitri@arrigoni.me>
2021-01-16 17:14:29 +0100

    config (spec) introducing samples tests

--- spec/stibium/bundled/bundle/config_spec.rb
+++ spec/stibium/bundled/bundle/config_spec.rb
@@ -44,3 +44,25 @@ describe Stibium::Bundled::Bundle::Config, :'stibium/bundled/bundle/config' do
     end
   end
 end
+
+# samples -----------------------------------------------------------
+sham!(:'samples/configs').lister.call.tap do |samples|
+  {
+    'BUNDLE_APP_CONFIG' => '.bundle',
+    'BUNDLE_PATH' => 'bundle'
+  }.map { |k, v| [k.freeze, v.freeze] }.to_h.yield_self do |defaults|
+    {
+      empty: defaults,
+      local: defaults.merge({ 'BUNDLE_FOO' => 'bar' }),
+    }
+  end.sort.to_h.freeze.each do |sample_name, expected|
+    # @type [Class<Stibium::Bundled::Bundle::Config>] described_class
+    # @type [Stibium::Bundled::Bundle::Config] subject
+    describe Stibium::Bundled::Bundle::Config, :'stibium/bundled/bundle/config', :samples do
+      let(:sample) { samples.fetch(sample_name) }
+      let(:subject) { described_class.new(sample.basedir, env: sample.env) }
+
+      it { expect(subject).to eq(expected) }
+    end
+  end
+end
