49c53b067deaebd51dcb17ded9b0d88e6a496a20
Dimitri Arrigoni <dimitri@arrigoni.me>
2021-01-17 13:14:38 +0100

    sham (spec) samples/bundles improvements

--- spec/sham/samples/bundles.rb
+++ spec/sham/samples/bundles.rb
@@ -1,23 +1,39 @@
 # frozen_string_literal: true
 
+results = {
+  empty: [FalseClass, NilClass],
+  gemfile: [TrueClass, Stibium::Bundled::Bundle],
+  gemfile_old: [TrueClass, Stibium::Bundled::Bundle],
+  partial: [FalseClass, NilClass],
+  partial_old: [FalseClass, NilClass],
+  standalone: [TrueClass, Stibium::Bundled::Bundle]
+}
+
 lister = lambda do
-  Dir.glob("#{SAMPLES_PATH}/bundles/*")
-     .map { |fp| Pathname.new(fp) }
-     .keep_if(&:directory?)
-     .map { |fp| [fp.basename.to_s.to_sym, fp] }
-     .to_h
+  Dir.glob("#{SAMPLES_PATH}/bundles/*").map { |fp| Pathname.new(fp) }.keep_if(&:directory?).map do |fp|
+    [
+      fp.basename.to_s.to_sym,
+      {
+        basedir: fp,
+        env: {},
+        results: results.fetch(fp.basename.to_s.to_sym),
+      }.tap do |h|
+        h[:builder] = lambda do |base: Class|
+          base.new do
+            class << self
+              include Stibium::Bundled
+            end
+
+            self.bundled_from(fp, env: h.fetch(:env))
+          end
+        end
+      end.yield_self { |h| Struct.new(*h.keys).new(*h.values) }
+    ]
+  end.sort.to_h
 end
 
 builder = lambda do |name, base: Class|
-  lister.call.fetch(name.to_s.to_sym).yield_self do |basedir|
-    base.new do
-      class << self
-        include Stibium::Bundled
-      end
-
-      self.bundled_from(basedir)
-    end
-  end
+  lister.call.fetch(name.to_s.to_sym).builder.call(base: base)
 end
 
 {
